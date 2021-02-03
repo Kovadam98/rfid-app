bb43bf3c4be813b6fd6a9a338e137949f8f79a44
Adam Kovacs <kovadam98@gmail.com>
2021-02-03 10:12:37 +0100

    Add hash router.

--- rfid-app-frontend/src/components/App.tsx
+++ rfid-app-frontend/src/components/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
+import { HashRouter, Redirect, Route, Switch} from 'react-router-dom';
 import OrderPage from "../pages/OrderPage";
 import ProductionPage from '../pages/ProductionPage';
 
 export default function App() {
   return (
-        <BrowserRouter>
+        <HashRouter basename='/'>
           <Switch>
               <Route path='/production'>
                   <ProductionPage />
@@ -15,6 +15,6 @@ export default function App() {
               </Route>
               <Redirect to='/order' />
           </Switch>
-        </BrowserRouter>
+        </HashRouter>
   );
 }
